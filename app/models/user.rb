23681ab781dd21de6efbfdfcec64117a58b541b5
nakatanikodai <nakatanixx1123@gmail.com>
2020-04-19 19:09:00 +0900

    リファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,15 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :authentication_keys => [:user_id]
 
-  # user_idを必須、一意とする
   validates_uniqueness_of :user_id
   validates_presence_of :user_id
 
-  # user_idを仕様してログインするようオーバーライド
+  
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
     if login = conditions.delete(:login)
-      # 認証の条件式を変更する
+      
       where(conditions).where(["user_id = :value", {:value => user_id}]).first
     else
       where(conditions).first
